7d13a65114f8c7280638e36a75e608076bf65edf
Mikel <mikelsanvi@gmail.com>
2024-04-11 13:28:36 +0200

    quitados sudo

--- install.sh
+++ install.sh
@@ -13,26 +13,26 @@ snap install android-studio --classic
 snap install intellij-idea-community --classic
 snap install dbeaver-ce
 snap install postman
-sudo snap install drawio
+snap install drawio
 snap install --classic code
 
 # DOCKER
 
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
 
 addgroup docker
-whoami | xargs -I % sudo adduser % docker
+whoami | xargs -I % adduser % docker
 
 
 # CHROME
