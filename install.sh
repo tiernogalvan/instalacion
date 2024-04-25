075034107e422a7a1e60b452624f5c20745e2f4d
santigz <219253+santigz@users.noreply.github.com>
2024-04-26 00:12:27 +0200

    Add hostname

--- install.sh
+++ install.sh
@@ -3,17 +3,33 @@
 die() { echo "$*" 1>&2 ; exit 1; }
 
 if [[ "$EUID" -ne 0 ]]; then
-  die "Please run as root"
+  die "Please run as root."
 fi
+
+clear
+
+echo
+echo
+echo "  _   _                                   _                  "
+echo " | |_(_) ___ _ __ _ __   ___   __ _  __ _| |_   ____ _ _ __  "
+echo " | __| |/ _ \ '__| '_ \ / _ \ / _\` |/ _\` | \ \ / / _\` | '_ \ "
+echo " | |_| |  __/ |  | | | | (_) | (_| | (_| | |\ V / (_| | | | |"
+echo "  \__|_|\___|_|  |_| |_|\___/ \__, |\__,_|_| \_/ \__,_|_| |_|"
+echo "                              |___/                          "
+echo
+echo "IES Enrique Tierno Galván"
+echo "Asistente de instalación de equipo."
+echo
 
 # Ejecuta la instalación desde el propio directorio
 run_install() {
-  pushd .
+  pushd . >/dev/null
   cd scripts/$1
-  ./install.sh
-  popd
+  bash ./install.sh
+  popd >/dev/null
 }
 
+run_install hostname  # Must be first
 run_install apt
 run_install ssh
 run_install ldap
---
+++ scripts/hostname/install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Pregunta el nombre de aula y de puesto del equipo.
+# Establece el hostname.
+# Envía los datos al servidor junto a la MAC del equipo.
+#
+
+aula=""
+while [[ ! $aula ]]; do
+  read -p "Indica el aula (B15, B17, B21, B22...): " aula
+  aula=$(echo $aula | tr '[:lower:]' '[:upper:]')
+  [[ $aula =~ ^[0-9][0-9]$ ]] && aula="B$aula"
+  case $aula in
+    B15|B17|B21|B22|B23|B24|B25|B27|B32) 
+      ;;
+    *)
+      echo "El aula $aula no está entre las contempladas: B15, B17, B21, B22, B23, B24, B25, B27, B32"
+      read -p "¿Seguro que quieres usar ${aula}? (y/n): " confirm
+      if [[ $confirm != 'y' ]]; then
+        aula=""
+        echo
+      fi
+      ;;
+  esac
+done
+
+puesto=""
+while [[ ! $puesto ]]; do
+  read -p "Indica el puesto (A1, A2... F3, F4): " puesto
+  puesto=$(echo $puesto | tr '[:lower:]' '[:upper:]')
+  if [[ ! $puesto =~ ^[A-F][0-5]$ ]]; then
+      read -p "Puesto $aula no contemplado. ¿Seguro que quieres usar ese id? (y/n): " confirm
+      if [[ $confirm != 'y' ]]; then
+        puesto=""
+        echo
+      fi
+  fi
+done
+exit
+
+hostname="${aula}-${puesto}"
+echo "Setting hostname $hostname"
+hostnamectl hostname $hostname
+
+# Send the mac address
+devs=$(ip route show default | awk '/default via [0-9\.]* dev/ {print $5}')
+for dev in $devs ; do
+  mac=$(cat /sys/class/net/${dev}/address)
+  curl "https://lan.tiernogalvan.es/hostname/${hostname}/${mac}"
+done
