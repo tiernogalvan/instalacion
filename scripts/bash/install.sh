742954542c95812ea90ec7e3712ab20457fc7378
santigz <219253+santigz@users.noreply.github.com>
2024-04-27 12:50:30 +0200

    Add bash/.gitconfig

--- scripts/bash/install.sh
+++ scripts/bash/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 install -o root -g root -m 0644 -t /etc/skel/ -D .bashrc
+install -o root -g root -m 0644 -t /etc/skel/ -D .gitconfig
 install -o root -g root -m 0644 -t /etc/profile.d/ -D default-umask.sh
