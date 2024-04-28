f516f8b0cdf957fd801a546eb39e254fe504960a
santigz <219253+santigz@users.noreply.github.com>
2024-04-28 08:56:33 +0200

    Update hostname

--- scripts/hostname/install.sh
+++ scripts/hostname/install.sh
@@ -4,44 +4,68 @@
 # Envía los datos al servidor junto a la MAC del equipo.
 #
 
-aula=""
-while [[ ! $aula ]]; do
-  # Forcing the read from tty allows this script to be run from a pipe
-  read -p "Indica el aula (B15, B17, B21, B22...): " aula < /dev/tty
-  [[ $aula ]] || continue
-  aula=$(echo $aula | tr '[:lower:]' '[:upper:]')
-  [[ $aula =~ ^[0-9][0-9]$ ]] && aula="B$aula"
-  case $aula in
-    B15|B17|B21|B22|B23|B24|B25|B27|B32) 
-      ;;
-    *)
-      echo "El aula $aula no está entre las contempladas: B15, B17, B21, B22, B23, B24, B25, B27, B32"
-      read -p "¿Seguro que quieres usar ${aula}? (y/n): " confirm < /dev/tty
-      if [[ $confirm != 'y' ]]; then
-        aula=""
-        echo
-      fi
-      ;;
-  esac
-done
+read_aula() {
+  aula=""
+  while [[ ! $aula ]]; do
+    # Forcing the read from tty allows this script to be run from a pipe
+    read -p "Indica el aula (B15, B17, B21, B22...): " aula < /dev/tty
+    [[ $aula ]] || continue
+    aula=$(echo $aula | tr '[:lower:]' '[:upper:]')
+    [[ $aula =~ ^[0-9][0-9]$ ]] && aula="B$aula"
+    case $aula in
+      B15|B17|B21|B22|B23|B24|B25|B27|B32) 
+        ;;
+      *)
+        echo "El aula debería ser: B15, B17, B21, B22, B23, B24, B25, B27, B32" > /dev/tty
+        read -p "¿Seguro que quieres usar ${aula}? (y/n): " confirm < /dev/tty
+        if [[ $confirm != 'y' ]]; then
+          aula=""
+          echo > /dev/tty
+        fi
+        ;;
+    esac
+  done
+  echo $aula
+}
 
-puesto=""
-while [[ ! $puesto ]]; do
-  read -p "Indica el puesto (A1, A2... F3, F4): " puesto < /dev/tty
-  [[ $puesto ]] || continue
-  puesto=$(echo $puesto | tr '[:lower:]' '[:upper:]')
-  if [[ ! $puesto =~ ^[A-F][0-5]$ ]]; then
-      read -p "Puesto $puesto no contemplado. ¿Seguro que quieres usar ese id? (y/n): " confirm < /dev/tty
-      if [[ $confirm != 'y' ]]; then
-        puesto=""
-        echo
-      fi
-  fi
-done
+read_puesto() {
+  puesto=""
+  while [[ ! $puesto ]]; do
+    read -p "Indica el puesto (A1, A2... F3, F4): " puesto < /dev/tty
+    [[ $puesto ]] || continue
+    puesto=$(echo $puesto | tr '[:lower:]' '[:upper:]')
+    if [[ ! $puesto =~ ^[A-F][0-5]$ ]]; then
+        read -p "Puesto no contemplado. ¿Seguro que quieres usar ${puesto}? (y/n): " confirm < /dev/tty
+        if [[ $confirm != 'y' ]]; then
+          puesto=""
+          echo > /dev/tty
+        fi
+    fi
+  done
+  echo $puesto
+}
 
-hostname="${aula}-${puesto}"
-echo "Setting hostname $hostname"
-hostnamectl hostname $hostname
+hostname_is_ok() {
+  [[ $(hostname | grep -E '^B[0-9]{2}-[A-F][0-5]$') ]] && echo $(hostname)
+}
+
+set_hostname=true
+if [[ $(hostname_is_ok) ]]; then
+  echo "Equipo detectado."
+  echo "  Aula: $(hostname | cut -d'-' -f1)"
+  echo "  Puesto: $(hostname | cut -d'-' -f2)"
+  read -p "¿Es correcto? (y/n): " correcto
+  [[ $correcto = 'y' ]] && unset set_hostname
+fi
+
+if [[ $set_hostname ]]; then
+  aula=$(read_aula)
+  puesto=$(read_puesto)
+  hostname="${aula}-${puesto}"
+  echo "Setting hostname $hostname"
+  hostnamectl hostname $hostname
+fi
+
 
 # Send the mac address
 devs=$(ip route show default | awk '/default via [0-9\.]* dev/ {print $5}')
