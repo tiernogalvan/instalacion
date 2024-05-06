1f37f4aaf53a1917a41e12ec78e97297ff93e6d7
santigz <219253+santigz@users.noreply.github.com>
2024-05-07 01:20:42 +0200

    Update ldap - install sssd-tools

--- scripts/ldap/install.sh
+++ scripts/ldap/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
 # Instala SSSD para tener login al servidor LDAP
+# Para vaciar la cache de SSSD:
+#   $ sss_cache -E
+#   $ sss_cache -u diurno
 #
 
-apt-get install -y sssd-ldap ldap-utils libsss-sudo
+apt-get install -y sssd-ldap ldap-utils libsss-sudo sssd-tools
 install -o root -g root -m 0600 -t /etc/sssd/ -D sssd.conf
 systemctl restart sssd.service
 pam-auth-update --enable mkhomedir
@@ -12,3 +15,4 @@ pam-auth-update --enable mkhomedir
 # como ldapsearch, ldapwhoami...
 mkdir -p /etc/ldap
 install -o root -g root -m 0644 -t /etc/ldap/ -D ldap.conf
+
