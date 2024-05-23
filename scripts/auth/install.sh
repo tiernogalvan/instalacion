7fa970a8096a58b0157cd2e007b700471fcd4adb
santigz <219253+santigz@users.noreply.github.com>
2024-05-23 13:29:35 +0200

    Custom message at forbidden auth login time

--- scripts/auth/install.sh
+++ scripts/auth/install.sh
@@ -7,13 +7,20 @@
 
 ensure_line_in_file () {
   file="$1"
-  line="$2"
+  pattern="$2"
+  line="$3"
   # Use -F to match literal string, not regex
-  if [[ ! $(grep -F "$line" "$file") ]]; then
+  if [[ ! $(grep -F "$pattern" "$file") ]]; then
     echo "$line" >> $file
   fi
 }
 
-ensure_line_in_file /etc/security/time.conf   '* ; * ; diurno ; Al0800-1500'
-ensure_line_in_file /etc/security/time.conf   '* ; * ; vespertino ; Al1500-2200'
-ensure_line_in_file /etc/pam.d/common-account 'account required pam_time.so'
+ensure_line_in_file /etc/security/time.conf diurno     '* ; * ; diurno     ; Al0800-1500'
+ensure_line_in_file /etc/security/time.conf vespertino '* ; * ; vespertino ; Al1500-2200'
+
+# Aquí activamos pam_time.
+# En caso login denegado ejecutamos tierno_pam_time_denied.sh para mostrar mensaje de error personalizado.
+# `success=1` ignora la siguiente línea en caso de login exitoso. Ver `man pam.conf`
+install -o root -g root -m 0644 -t /usr/local/sbin/ -D tierno_pam_time_denied.sh
+ensure_line_in_file /etc/pam.d/common-account pam_time.so 'account [success=1 new_authtok_reqd=ok ignore=ignore default=bad] pam_time.so debug'
+ensure_line_in_file /etc/pam.d/common-account tierno_pam_time_denied.sh 'account [default=ignore] pam_exec.so stdout /usr/local/sbin/tierno_pam_time_denied.sh'
