055ff2ec2596aca1e8502aa97ebbfa63b03f709d
Santi Gonzalez <219253+santigz@users.noreply.github.com>
2024-06-03 18:44:25 +0200

    Fix veyon install

--- scripts/veyon/install.sh
+++ scripts/veyon/install.sh
@@ -9,7 +9,7 @@ ensure_line_in_file /etc/gdm3/custom.conf WaylandEnable 'WaylandEnable=false'
 
 add-apt-repository -y ppa:veyon/stable
 apt-get remove -y veyon-*
-apt-get install -y veyon veyon-*
+apt-get install -y veyon
 
 veyon-cli config import ./veyon-config-client.json
 
