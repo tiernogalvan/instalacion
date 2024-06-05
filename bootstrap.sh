dc1781cd635762ce562d7c26e6db9fdc41046795
Mikel <mikelsanvi@gmail.com>
2024-06-05 14:11:22 +0200

    reinicio por defecto

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ unzip -q main.zip
 cd instalacion-main
 
 # Redirecting to tty avoids with pipes: cat boostrap.sh | bash
-time bash ./install.sh < /dev/tty > /dev/tty
+time bash ./install.sh -r < /dev/tty > /dev/tty
 
 # Post-cleanup
 rm -f /root/main.zip
