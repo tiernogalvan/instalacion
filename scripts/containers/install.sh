ba68769fd42d06b7fc714373e8777af38d2b9796
santigz <219253+santigz@users.noreply.github.com>
2024-10-15 18:59:49 +0200

    Mysql subido a 8.1

--- scripts/containers/install.sh
+++ scripts/containers/install.sh
@@ -2,33 +2,33 @@
 
 # MySQL
 
-if [[ ! $(docker ps --all | grep diurno-mysql) ]]; then
-  PASSWORD=Sandia4you
-  # Este comando lanza un contenedor con MySQL llamado diurno-mysql, la clave de root es Sandia4you
-  docker run --name diurno-mysql -e MYSQL_ROOT_PASSWORD=$PASSWORD -p 3306:3306 -d mysql:8.0.0
-  docker stop diurno-mysql
-fi
+function create_mysql() {
+  container_name="$1"
+  password="$2"
+  if [[ ! $(docker ps --all | grep $container_name) ]]; then
+    # Este comando lanza un contenedor con MySQL llamado diurno-mysql, la clave de root es Sandia4you
+    # docker run --name diurno-mysql -e MYSQL_ROOT_PASSWORD=$password -p 3306:3306 -d mysql:8.0.0
 
-if [[ ! $(docker ps --all | grep vespertino-mysql) ]]; then
-  PASSWORD=Tokio2324
-  # Este comando lanza un contenedor con MySQL llamado vespertino-mysql, la clave de root es Tokio2324
-  docker run --name vespertino-mysql -e MYSQL_ROOT_PASSWORD=$PASSWORD -p 3306:3306 -d mysql:8.0.0
-  docker stop vespertino-mysql
-fi
+    # MySQL 8.0 ignora los CHECK al crear tablas
+    # MySQL 8.1 ya no soporta passwords normales por defecto, hay que pasarle el parámetro siguiente.
+    docker run --name $container_name -e MYSQL_ROOT_PASSWORD=$password -p 3306:3306 -d mysql:8.1 mysqld --default-authentication-plugin=mysql_native_password
+    docker stop $container_name
+  fi
+}
+
+create_mysql diurno-mysql Sandia4you
+create_mysql vespertino-mysql Tokio2324
 
 # PostgreSQL
 
-if [[ ! $(docker ps --all | grep diurno-postgres) ]]; then
-  PASSWORD=Sandia4you
-  # Este comando lanza un contenedor con Postgresql llamado diurno-postgres, la clave de root es Sandia4you
-  docker run --name diurno-postgres -e POSTGRES_PASSWORD=$PASSWORD -p 5432:5432  -d postgres:14.0
-  docker stop diurno-postgres
-fi
+function create_postgress() {
+  container_name="$1"
+  password="$2"
+  if [[ ! $(docker ps --all | grep $container_name) ]]; then
+    docker run --name $container_name -e POSTGRES_PASSWORD=$password -p 5432:5432  -d postgres:14.0
+    docker stop $container_name
+  fi
+}
 
-if [[ ! $(docker ps --all | grep vespertino-postgres) ]]; then
-  PASSWORD=Tokio2324
-  # Este comando lanza un contenedor con Postgresql llamado vespertino-postgres, la clave de root es Tokio2324
-  docker run --name vespertino-postgres -e POSTGRES_PASSWORD=$PASSWORD -p 5432:5432  -d postgres:14.0
-  docker stop vespertino-postgres
-fi
-
+create_postgress diurno-postgres Sandia4you
+create_postgress vespertino-postgres Tokio2324
