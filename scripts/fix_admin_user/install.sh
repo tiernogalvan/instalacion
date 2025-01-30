c0ec9f486dea240f6bd335a1438d6b2e83dcc833
Mikel <mikel.san@educa.madrid.org>
2025-01-30 14:02:45 +0100

    arreglado renombrado

--- scripts/fix_admin_user/install.sh
+++ scripts/fix_admin_user/install.sh
@@ -3,10 +3,11 @@
 # This script should be run in a fresh installation of Ubuntu 22.04
 #
 
-if [[ $USER != "administrator" ]]; then
+if [[ $SUDO_USER != "administrator" ]]; then
 
-	echo Nombre de usuario $USER incorrecto, renombrando a administrator
+	echo Nombre de usuario $SUDO_USER incorrecto, renombrando a administrator
+	systemc
 	groupadd administrator
-	usermod -d /home/administrator -m -g administrator -l administrator $USER
+	usermod -d /home/administrator -m -g administrator -l administrator $SUDO_USER
 
 fi
