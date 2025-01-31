832e331b49b345160b37355032ec81a003025fef
Mikel <mikel.san@educa.madrid.org>
2025-01-31 13:55:13 +0100

    error corregido

--- install.sh
+++ install.sh
@@ -22,9 +22,9 @@ echo "Asistente de instalación de equipo."
 echo
 
 
-if [[ $SUDO_USER != "administrator" ]]; then
-  echo Este script debe ejecutarse con el usuario administrator (usuario actual $SUDO_USER)
-  echo Si durante la instalación de Ubuntu se crea un usuario erróneo, sigue las instrucciones del punto Arreglar nombre del usuario administrator del README de este repositorio
+if [[ ${SUDO_USER} != "administrator" ]]; then
+  echo "Este script debe ejecutarse con el usuario administrator (usuario actual $SUDO_USER)"
+  echo "Si durante la instalación de Ubuntu se crea un usuario erróneo, sigue las instrucciones del punto Arreglar nombre del usuario administrator del README de este repositorio"
 else
   rootpath="$(pwd)"
   run_install() {
