1d6c4ae4dae0cedc6914bc2663e66d0ac6a018f9
Mikel <mikel.san@educa.madrid.org>
2025-02-03 14:17:17 +0100

    Mejorado patrón de fix_administrator

--- fix_administrator.sh
+++ fix_administrator.sh
@@ -3,7 +3,7 @@
 # This script should be run in a fresh installation of Ubuntu 22.04
 #
 
-BAD_NAME=$(cat /etc/passwd | grep 1000 | awk -F : '{ print $1 }')
+BAD_NAME=$(cat /etc/passwd | grep :1000:1000: | awk -F : '{ print $1 }')
 
 echo "Se va a renombrar el usuario ${BAD_NAME} a administrator"
 
