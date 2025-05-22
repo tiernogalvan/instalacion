61e3e93262dcbe3f5e88d8758e368a42ba237109
santigz <219253+santigz@users.noreply.github.com>
2025-05-22 12:12:38 +0200

    Fix ssh

--- scripts/ssh/install.sh
+++ scripts/ssh/install.sh
@@ -6,5 +6,5 @@ install -o root -g root -m 0600 -t /etc/ssh/sshd_config.d/ -D sshd_tierno.conf
 
 # Por algún motivo es necesario reinstalar en ubuntu 24
 apt install --reinstall openssh-server
-systemctl enable sshd.service
-systemctl restart sshd.service
+systemctl enable ssh.service
+systemctl restart ssh.service
