dbafde5bb0e33a5c4f98d814cbba3d0b33f391c9
santigz <219253+santigz@users.noreply.github.com>
2025-05-23 11:11:24 +0200

    Update wake-on-lan

--- scripts/wake-on-lan/wol-netplan.sh
+++ scripts/wake-on-lan/wol-netplan.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-# Este script activa Wake On Lan en sistemas con Netplan (ej Ubuntu)
 
-interfaces=$(nmcli device status | grep ethernet | awk '{print $1}')
+echo "Habilitando Wake-on-LAN..."
 
-if [ -z "$interfaces" ]; then
-  echo "No Ethernet interfaces found."
-  exit 1
+# Get a list of all Ethernet connection names.
+readarray -t connection_list < <(nmcli -t -f NAME,TYPE connection show | grep ethernet | awk -F: '$2 ~ /ethernet/ {print $1}')
+
+if [ ${#connection_list[@]} -eq 0 ]; then
+  echo "No Ethernet connections found. Exiting."
+  exit 0
 fi
 
-for interface in $interfaces; do
-  connection=$(nmcli connection show | grep "$interface" | awk '{print $1}')
-
-  if [ -z "$connection" ]; then
-    echo "No connection found for interface $interface."
-    continue
-  fi
-
-  echo "Enabling Wake-on-LAN for $connection..."
-
-  nmcli connection modify "$connection" 802-3-ethernet.wake-on-lan magic
-
-  nmcli connection down "$connection"
-  nmcli connection up "$connection"
-
-  if ethtool "$interface" | grep -q "Wake-on: g"; then
-    echo "Wake-on-LAN enabled for $interface"
+for connection in "${connection_list[@]}"; do
+  interface=(nmcli -t -f connection.interface-name connection show "$connection" | awk -F: '{print $2}')
+  if nmcli connection modify "$connection" 802-3-ethernet.wake-on-lan magic; then
+    echo "Wake-on-LAN activado para $interface - $connection."
   else
-    echo "Failed to enable Wake-on-LAN for $interface"
+    echo "Fallo activando Wake-on-LAN para conexion $interface - $connection."
   fi
 done
+
+# Para listar conextiones:
+# nmcli connection
+#
+# Para verificar WOL activado:
+# nmcli connection show <connection_name> | grep wake-on-lan"
+# ethtool enp2s0
