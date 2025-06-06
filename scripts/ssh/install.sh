9c060a458a5b716eed9a2fb96f6d081df2baf378
santigz <219253+santigz@users.noreply.github.com>
2025-06-06 09:22:38 +0200

    Update ssh

--- scripts/ssh/install.sh
+++ scripts/ssh/install.sh
@@ -5,9 +5,9 @@ install -o root -g root -m 0600 -t /root/.ssh/ -D authorized_keys
 install -o root -g root -m 0600 -t /etc/ssh/sshd_config.d/ -D sshd_tierno.conf
 
 # Administrator key
-#if [[ $(id administrator) ]]; then
-#  install -o administrator -g administrator -m 0600 -t /home/administrator/.ssh/authorized_keys -D authorized_keys_administrator
-#fi
+if [[ $(id administrator) ]]; then
+  install -o administrator -g administrator -m 0600 -t /home/administrator/.ssh/ -D authorized_keys_administrator authorized_keys
+fi
 
 # Por algún motivo es necesario reinstalar en ubuntu 24
 apt install --reinstall openssh-server
