28624d1d64a1592b99b89dcf987290228449f412
Mikel <mikel.san@educa.madrid.org>
2026-04-15 12:48:22 +0200

    no se elimina thunderbird

--- scripts/apps/install.sh
+++ scripts/apps/install.sh
@@ -37,7 +37,7 @@ ttf-mscorefonts-installer
 composer
 "
 apt-get install -y $PACKAGES
-apt-get purge -y aisleriot gnome-mahjongg gnome-mines gnome-sudoku thunderbird
+apt-get purge -y aisleriot gnome-mahjongg gnome-mines gnome-sudoku
 apt-get upgrade -y
 
 # PHP
